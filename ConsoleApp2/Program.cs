3c24546c2b4a897a427f9843447879766715a21c
azadei <azadei@code.edu.az>
2023-07-06 15:26:32 +0400

    1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        DateTime date = new DateTime(1992, 06, 16);
+
+        DateTime newdate = date.AddDays(30).AddHours(6).AddMinutes(17);
+        Console.WriteLine(newdate);
+
+
+
+
+
+
+
+    }
+}
+    
+
+
+
+
+
+
+
